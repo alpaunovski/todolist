dffebc0a00a14a063395c9adc6c913c1c4032558
alpaunovski <aleksandar@paunovsky.com>
2021-04-25 17:23:36 +0300

    Added a /work route and a second /work list

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const https = require("https");
 app.use(express.static("public"));
 var items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,18 +22,37 @@ app.get("/", function(req, res) {
     var day = today.toLocaleDateString("en-US", options);
 
     var lastItem = items[items.length - 1];
-    res.render("list", { kindOfDay: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
 
 
 });
 
 app.post("/", function(req, res) {
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
-    items.push(item);
-    res.redirect("/");
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+
+    } else {
+        items.push(item);
+        res.redirect("/");
+
+    }
+
 });
 
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+
+    workItems.push(item);
+    res.redirect("/work");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
