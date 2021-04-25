9f8f0e315084e3a02c9c07a8522c90fec76cd44e
alpaunovski <aleksandar@paunovsky.com>
2021-04-25 17:41:42 +0300

    Added an /about route. Split the header and the footer into seprarate files.

--- app.js
+++ app.js
@@ -54,6 +54,9 @@ app.post("/work", function(req, res) {
     res.redirect("/work");
 });
 
+app.get("/about", function(req, res) {
+    res.render("about");
+});
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
